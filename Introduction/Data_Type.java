6bcd212047d8d20896a177efdd2f207e607afbb9
nimamakhmali <nmkhmly790@gmail.com>
2025-01-03 17:19:49 +0100

    data types

---
+++ Introduction/Data_Type.java
@@ -0,0 +1,21 @@
+public class Data_Type {
+    public static void main(String[] args) {
+
+        int age = 20;
+        long population = 7800000000L;
+
+        float height = 5.7f;
+        double weight = 68.5;
+
+        char grade = 'N';
+        String name = "Nima";
+
+  
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
