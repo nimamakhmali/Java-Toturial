4f43d903f9efb5ec99c1636d43e24d9cfe7da3df
nimamakhmali <nmkhmly790@gmail.com>
2025-01-03 17:39:03 +0100

    operators

---
+++ Introduction/operators/Comparsion_Operators.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class Comparsion_Operators {
+    public static void main(String[] args) {
+        int x = 5, y = 3;
+        
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+    }
+    
+}
---
+++ Introduction/operators/Logical_Operators.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class Logical_Operators{
+    public static void main(String[] args) {
+        boolean isJavaFun = true, isFishTasty = false;
+
+        System.out.println("isJavaFun && isFishTasty : " + (isJavaFun && isFishTasty));
+        System.out.println("isJavaFun || isFishTasty : " + (isJavaFun || isFishTasty));
+        System.out.println("!isJavaFun : " + (!isJavaFun));
+    }
+}
